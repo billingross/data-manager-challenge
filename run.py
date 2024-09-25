632b6d84cb7a270bffa96f0fffa4c3b404271df2
Paul Billing-Ross <billingross@gmail.com>
2024-09-25 15:20:25 -0700

    Change applymap to map

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ import gzip
 
 import pandas as pd
 
+from pathlib import Path
 from functools import reduce
 
 def get_allele_count(genotype, allele):
@@ -15,7 +16,6 @@ def get_allele_count(genotype, allele):
 	return alleles.count(str(allele))
 
 info_columns = ['#CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT']
-
 input_directory = "exercise_input_data_public"
 
 tsv_files =  glob.glob(f"{input_directory}/*.tsv.gz")
@@ -37,23 +37,34 @@ cleaned_df = reduce_merged_df.rename(columns={'HG10101':'HG00101'})
 genotypes_df = cleaned_df.filter(regex='HG')
 
 # Convert genotypes to major and minor allele counts per each site and sample
-major_ac_df = genotypes_df.applymap(get_allele_count, allele='0')
-minor_ac_df = genotypes_df.applymap(get_allele_count, allele='1')
+major_ac_df = genotypes_df.map(get_allele_count, allele='0')
+minor_ac_df = genotypes_df.map(get_allele_count, allele='1')
 
 # Sum major and minor allele counts across sites (per sample)
 sum_major_ac_df = major_ac_df.apply(sum, axis=0)
 sum_minor_ac_df = minor_ac_df.apply(sum, axis=0)
 
 # Combine major and minor allele counts into single dataframe
-allele_count_df = pd.concat([sum_major_ac_frame, sum_minor_ac_frame], axis=1)
+allele_count_df = pd.concat([sum_major_ac_df, sum_minor_ac_df], axis=1)
 allele_count_df.columns = ['major_count', 'minor_count']
 
 # Write allele counts dataframe to TSV
 allele_count_df.to_csv("allele_counts.tsv", sep='\t', index_label="sample_id")
 ###
 
-"""
-Ouput:
-b'#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tHG00145\n'
-b'21\t10000025\trs530469795\tTA\tT\t100\tPASS\tAC=41;AF=0.0081869;AN=5008;NS=2504;DP=17454;EAS_AF=0.005;AMR_AF=0;AFR_AF=0.0265;EUR_AF=0;SAS_AF=0.001;AA=|||unknown(NO_COVERAGE);VT=INDEL\tGT\t0|0\n'
-"""
+
+### Subset and save new files
+# Create output directory
+Path("output").mkdir(parents=False, exist_ok=True)
+
+# Subset data and write to new files
+variant_info_df = cleaned_df.iloc[:, :9]
+for i in range(0,10):
+	sample_subset_df = cleaned_df.filter(regex=f"{i}$")
+	subset_df = pd.concat([variant_info_df, sample_subset_df], axis=1)
+	print(f"Subset dataframe shape: {subset_df.shape}.")
+	subset_df.to_csv(
+					 f"output/{i}.chr21.10000000_14999999.tsv.gz",
+					 sep='\t',
+					 index=False)
+###
