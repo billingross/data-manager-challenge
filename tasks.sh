2fba4e9edcbdb8430c00abd499131eb73ad40fe5
Paul Billing-Ross <billingross@gmail.com>
2024-09-25 15:15:17 -0700

    Remove tsv output

--- tasks.sh
+++ tasks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for filename in input/*.tsv.gz; do
+for filename in output/*.tsv.gz; do
 	echo ${filename}
 	last_digit=$(echo $(basename ${filename}) | awk -F '.' '{print $1}')
 	chromosome=$(echo $(basename ${filename}) | awk -F '.' '{print $2}')
@@ -9,7 +9,6 @@ for filename in input/*.tsv.gz; do
 	for ((i=10;i<=14;i++)); do
 		start=$(( i * 1000000 ))
     	end=$(( start + 999999 ))
-		#gzip -d -c "${filename}" | awk -v start=$start -v end=$end -F '\t' '(($2>=start && $2<=end) || $2=="POS") {print}' > "tasks_output/${last_digit}.chr21_task.${start}_${end}.tsv"
 		gzip -d -c "${filename}" | awk -v start=$start -v end=$end -F '\t' '(($2>=start && $2<=end) || $2=="POS") {print}' | gzip > "tasks_output/${last_digit}.${chromosome}.${start}_${end}.tsv.gz"
 	done
 done
